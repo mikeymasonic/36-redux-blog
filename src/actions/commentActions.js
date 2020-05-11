df21ca962eb5fb2986227f50dfca95d3aca02de0
Michael Romay <amerror@gmail.com>
2020-05-11 16:44:17 -0700

    wrote action for DELETE_COMMENT

--- src/actions/commentActions.js
+++ src/actions/commentActions.js
@@ -3,3 +3,9 @@ export const createComment = (comment, index) => ({
   type: CREATE_COMMENT,
   payload: comment, index
 });
+
+export const DELETE_COMMENT = 'DELETE_COMMENTS';
+export const deleteComment = (comment, index) => ({
+  type: DELETE_COMMENT,
+  payload: comment, index
+});
