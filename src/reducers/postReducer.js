6eb70bc61425bfa3b8acf2bf9f2293d96b95052d
Michael Romay <amerror@gmail.com>
2020-05-11 16:28:30 -0700

    wrote UPDATE_POST reducer

--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { DELETE_POST, ADD_POST } from '../actions/postActions';
+import { DELETE_POST, ADD_POST, UPDATE_POST } from '../actions/postActions';
 
 const initialState = [];
 
@@ -6,8 +6,21 @@ export default function reducer(state = initialState, action) {
   switch(action.type) {
     case ADD_POST:
       return [...state, action.payload];
+
+    case UPDATE_POST:
+      return state.map((post, i) => {
+        if(i === action.payload.index) {
+          return {
+            title: action.payload.title,
+            body: action.payload.body
+          };
+        }
+        return post;
+      });
+
     case DELETE_POST:
       return state.filter((_, i) => i !== action.payload);
+      
     default:
       return state;
   }
