a667cd6df8f5aa2ad396008a3918dd87f23f3440
Michael Romay <amerror@gmail.com>
2020-05-11 16:19:13 -0700

    wrote postActions for UPDATE_POST

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,7 +1,17 @@
-export const ADD_POST = 'ADD_Post';
+export const ADD_POST = 'ADD_POST';
 export const addPost = post => ({
   type: ADD_POST,
   payload: post
+});
+
+export const UPDATE_POST = 'UPDATE_POST';
+export const updatePost = (index, title, body) => ({
+  type: UPDATE_POST,
+  payload: {
+    index,
+    title,
+    body
+  }
 });
 
 export const DELETE_POST = 'DELETE_POST';
