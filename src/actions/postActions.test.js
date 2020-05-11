43360f1c72d12c001d8570d46c3bfc82d28c4040
Michael Romay <amerror@gmail.com>
2020-05-11 16:24:49 -0700

    wrote a test for UPDATE_POST action

--- src/actions/postActions.test.js
+++ src/actions/postActions.test.js
@@ -1,7 +1,7 @@
-import { addPost, ADD_POST, deletePost, DELETE_POST } from './postActions';
+import { addPost, ADD_POST, deletePost, DELETE_POST, updatePost, UPDATE_POST } from './postActions';
 
 describe('post actions', () => {
-  it('creates an ADD_POST action', () => {
+  it('creates a ADD_POST action', () => {
     const action = addPost({
       title: 'how I overcame my crippling fear of react',
       body: 'Just kidding, it\'s still there'
@@ -12,6 +12,19 @@ describe('post actions', () => {
       payload: {
         title: 'how I overcame my crippling fear of react',
         body: 'Just kidding, it\'s still there'
+      }
+    });
+  });
+
+  it('creates a UPDATE_POST action', () => {
+    const action = updatePost(2, 'This is a new title', 'This is a new body');
+
+    expect(action).toEqual({
+      type: UPDATE_POST,
+      payload: {
+        index: 2,
+        title: 'This is a new title',
+        body: 'This is a new body'
       }
     });
   });
